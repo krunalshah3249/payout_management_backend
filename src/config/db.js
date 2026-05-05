540125d1882bead66eeed9d708ffbe9a5683b62b
Krunal Shah <krunalshah3249@gmail.com>
2026-05-06 00:26:37 +0530

    code update

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
 const connectDb = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return mongoose.connection;
+  const mongoUri = process.env.MONGODB_URI;
+  if (!mongoUri) {
+    throw new Error("MONGODB_URI is not configured.");
   }
 
-  const mongoUri = "mongodb+srv://krunalshah3249_db_user:V94OuTh6KaxXYEzv@payoutmanage.qjwu9i3.mongodb.net/?appName=payoutmanage";
-  
-  try {
-    await mongoose.connect(mongoUri, {
-      serverSelectionTimeoutMS: 10000,
-    });
-    console.log("MongoDB connected successfully");
-    return mongoose.connection;
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message || error);
-    throw error;
-  }
+  await mongoose.connect(mongoUri);
 };
 
 module.exports = { connectDb };
